a696b4dfeb7765a74d1a2662f35ea6874601dcf1
Ryan Breaker <ryanbreaker@ryanbreaker.pw>
2015-05-14 17:50:02 -0500

    Proper product pulling from db

--- product.php
+++ product.php
@@ -1,5 +1,42 @@
 <?php
 require_once "db/mysql.php";
+
+$isbn = $_GET['isbn'];
+
+// If the ISBN is invalid...
+//if (!$isbn) {
+//    http_redirect("/");
+//    exit();
+//}
+
+$productStmt = $mysqli->prepare("
+SELECT
+  tblbooks.title,
+  tblbooks.price,
+  tblbooks.imageFilename,
+  tblbooks.description,
+  tblauthors.compositeName
+FROM tblbooks
+  JOIN tblbooksauthorsxref
+    ON tblbooksauthorsxref.isbn = tblbooks.isbn
+  JOIN tblauthors
+    ON tblbooksauthorsxref.authorId = tblauthors.authorId
+WHERE tblbooks.isbn = ?
+LIMIT 1
+");
+
+$productStmt->bind_param("s", $isbn);
+$productStmt->execute();
+
+// Bind and fetch results, then close the stmt
+$productStmt->bind_result($title, $price, $imageFilename, $description, $authorName);
+$productStmt->fetch();
+$productStmt->close();
+
+// Decode escpaed HTML tags
+$description = str_replace(array('&lt;', '&gt;'), array('<', '>'), $description);
+// Remove erroneous paragraphs
+$description = str_replace(array('<p style="margin: 0px;">&Acirc;&nbsp;</p>', '<p></p>','style="margin: 0px;"'), array(), $description);
 ?>
 
 <!DOCTYPE html>
@@ -9,7 +46,10 @@ require_once "db/mysql.php";
     <title>Volga</title>
     <link href="stylesheets/style.css" rel="stylesheet" type="text/css">
     <script src="//use.typekit.net/sam3app.js"></script>
-    <script>try{Typekit.load();}catch(e){}</script>
+    <script>try {
+            Typekit.load();
+        } catch (e) {
+        }</script>
     <meta name="keywords" content="Books">
     <meta name="Description" content="Awesome books!">
 </head>
@@ -23,7 +63,7 @@ require_once "db/mysql.php";
             <?php
             $genres = $mysqli->query("SELECT * FROM tblgenres");
 
-            while($genre = $genres->fetch_assoc()):?>
+            while ($genre = $genres->fetch_assoc()):?>
                 <li>
                     <a href="/search.php?genre=<?php echo $genre['genreId']?>">
                         <?php echo $genre['genreName'] ?>
@@ -43,8 +83,19 @@ require_once "db/mysql.php";
     </form>
 </header>
 
-<main>
+<main id="book-page">
+    <img id="book-cover" src="/images/<?php echo $imageFilename ?>">
+    <article id="book-details">
+        <h2><?php echo $title ?></h2>
 
+        <h3><?php echo $authorName ?></h3>
+
+        <h4><?php echo '$' . $price ?></h4>
+
+        <p>
+            <?php echo $description ?>
+        </p>
+    </article>
 </main>
 
 <footer>
